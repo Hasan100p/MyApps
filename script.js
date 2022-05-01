a25034e1068c4684fb42ab2c75880432a1b72f70
Hasan100p <94968442+Hasan100p@users.noreply.github.com>
2022-05-01 15:10:44 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+var myAge = (((new Date().getFullYear() - 2010)*365)+((new Date().getMonth() - 4)*30)+((new Date().getDate() - 28)))/365;
+document.getElementsByClassName("homepage-tanıtma")[0].innerHTML = `Selam, ben Hasan. ${myAge} yaşındayım ve buraya yaptığım oyunlar,siteler vb. şeyleri koymaktayım, aşağıdan bakabilirsiniz.`;
+
+function superbeatiful3dgame(){
+    window.location = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
+}
+
+function balloonshooter(){
+    window.location = "file:///C:/Users/USER/Bilgisayar/Desktop/Kodlama/Uygulamalarım%20HTML/balloonshooter/index.html"    
+}
+
+function flappybird(){
+    window.location = "file:///C:/Users/USER/Bilgisayar/Desktop/Kodlama/Uygulamalarım%20HTML/flappybird/index.html"
+}
+
+function pong(){
+    window.location = "file:///C:/Users/USER/Bilgisayar/Desktop/Kodlama/Uygulamalarım%20HTML/pong/index.html"
+}
+
+function shooter(){
+    window.location = "file:///C:/Users/USER/Bilgisayar/Desktop/Kodlama/Uygulamalarım%20HTML/shooter/index.html"
+}
+
+function snake(){
+    window.location = "file:///C:/Users/USER/Bilgisayar/Desktop/Kodlama/Uygulamalarım%20HTML/snake/index.html"
+}
+
+function tron(){
+    window.location = "file:///C:/Users/USER/Bilgisayar/Desktop/Kodlama/Uygulamalarım%20HTML/tron/index.html"
+}
+
+function xox(){
+    window.location = "file:///C:/Users/USER/Bilgisayar/Desktop/Kodlama/Uygulamalarım%20HTML/xox/index.html"
+}
